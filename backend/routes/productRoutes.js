75d1d235709e29ec11fb3d455918addbb95f575a
Dungbui2k3 <Dungbui29082003@gmail.com>
2025-07-22 07:07:38 +0700

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -6,8 +6,11 @@ const {
   getProductById,
 } = require("../controllers/productController");
 const auth = require("../middlewares/authentication");
+const reviewController = require('../controllers/reviewController');
 
 router.post("/create", createProducts);
 router.get("/all", getAllProducts);
 router.get("/:id", getProductById);
+router.get('/:productId/reviews', reviewController.getReviewsByProduct);
+router.post('/:productId/reviews', reviewController.addReview);
 module.exports = router;
